be213c0733b81f91c7c69befed23dde92f6502bc
Alexey Kunitsky <kunitsky.alexey@gmail.com>
2019-03-17 15:00:28 +1000

    Add navbar & logo

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,39 +10,47 @@
   <title>База данных «Аптека»</title>
 </head>
 <body>
-<div class='container' style='padding: 48px 16px 16px'>
-  <div class='columns'>
-    <div class='column is-narrow'>
-      <nav>
-        <ul>
-          <li style='margin-bottom: 0.5rem'>
-            <a href='/' class="button is-warning is-rounded">В главное меню</a>
-          </li>
-          <li style='margin-bottom: 0.5rem'>
-            <a href='/drugs' class="button is-warning is-rounded">Таблица препаратов</a>
-          </li>
-          <li style='margin-bottom: 0.5rem'>
-            <a href='/providers' class="button is-warning is-rounded">Таблица поставщиков</a>
-          </li>
-          <li style='margin-bottom: 0.5rem'>
-            <a href='/categories' class="button is-warning is-rounded">Таблица категорий</a>
-          </li>
-          <li>
-            <a href='/transactions' class="button is-warning is-rounded">Таблица продаж</a>
-          </li>
-        </ul>
-      </nav>
+<nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class='container'>
+    <div class="navbar-brand">
+      <a class="navbar-item" href="/">
+        <img src='/images/logo.svg' alt='Аптека'>
+      </a>
     </div>
 
-    <div class='column is-expanded'>
-      <div class='columns is-centered'>
-        <div class='column is-narrow'>
-          @section('title')
-            <h1 class='title appTitle'>Аптека</h1>
-          @show()
-          @yield('content')
-        </div>
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start">
+        <a href='/' class="navbar-item">
+          Главное меню
+        </a>
+
+        <a href='/drugs' class="navbar-item">
+          Таблица препаратов
+        </a>
+
+        <a href='/providers' class="navbar-item">
+          Таблица поставщиков
+        </a>
+
+        <a href='/categories' class="navbar-item">
+          Таблица категорий
+        </a>
+
+        <a href='/transactions' class="navbar-item">
+          Таблица транзакций
+        </a>
+
       </div>
+    </div>
+  </div>
+</nav>
+<div class='container' style='padding: 48px 16px 16px'>
+  <div class='columns is-centered'>
+    <div class='column is-narrow'>
+      @section('title')
+        <h1 class='title appTitle'>Аптека</h1>
+      @show()
+      @yield('content')
     </div>
   </div>
 </div>
